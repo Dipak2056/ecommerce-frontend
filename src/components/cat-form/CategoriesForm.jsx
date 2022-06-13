525556a36ab83f97dff82c921b723b45112c8e45
Dipak 2056 <paudelsantosh508@gmail.com>
2022-06-13 15:16:37 +1000

    ui of categories paused

--- src/components/cat-form/CategoriesForm.jsx
+++ src/components/cat-form/CategoriesForm.jsx
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Col, Form, Row } from "react-bootstrap";
 
+const initialState = {
+  parentCat: "",
+  catName: "",
+};
 export const CategoriesForm = () => {
+  const [form, setForm] = useState(initialState);
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    console.log(form);
+  };
   return (
     <div>
-      <Form className="py-5">
+      <Form className="py-5" onSubmit={handleOnSubmit}>
         <Row className="g-3">
           <Col md="5">
             <Form.Group controlId="formGridState">
-              <Form.Select defaultValue="Choose...">
+              <Form.Select
+                name="parentCat"
+                defaultValue="Choose..."
+                onChange={handleOnChange}
+              >
                 <option>Choose...</option>
                 <option>option 1</option>
                 <option>option 2</option>
@@ -17,7 +37,12 @@ export const CategoriesForm = () => {
             </Form.Group>
           </Col>
           <Col md="4">
-            <Form.Control name="catName" placeholder="category Name" />
+            <Form.Control
+              name="catName"
+              placeholder="category Name"
+              onChange={handleOnChange}
+              required
+            />
           </Col>
           <Col md="3">
             <Button type="submit">Add Category</Button>
