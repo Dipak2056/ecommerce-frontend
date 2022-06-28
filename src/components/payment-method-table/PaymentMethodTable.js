ea138d4a4400a39858f330135ea09cbf27fa3750
Dipak 2056 <paudelsantosh508@gmail.com>
2022-06-28 11:03:56 +1000

    redux intgration for getting payment methods cmpltd

--- src/components/payment-method-table/PaymentMethodTable.js
+++ src/components/payment-method-table/PaymentMethodTable.js
@@ -1,11 +1,13 @@
 import React, { useEffect } from "react";
 import { Button } from "react-bootstrap";
 import Table from "react-bootstrap/Table";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchPaymentMethods } from "../../pages/payment-method/paymentMethodAction";
 
 const PaymentMethodTable = () => {
   const dispatch = useDispatch();
+  const { paymentMethods } = useSelector((state) => state.paymentMethods);
+
   useEffect(() => {
     dispatch(fetchPaymentMethods());
   }, []);
@@ -20,21 +22,23 @@ const PaymentMethodTable = () => {
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>1</td>
-          <td>inactive</td>
-          <td>Pay by credit card</td>
-          <td>
-            <Button variant="warning">
-              <i className="fa-solid fa-pencil"></i>
-              Edit
-            </Button>{" "}
-            <Button variant="danger">
-              <i className="fa-solid fa-trash-can"></i>
-              Delete
-            </Button>
-          </td>
-        </tr>
+        {paymentMethods.map((item, i) => (
+          <tr key={i}>
+            <td>{i}</td>
+            <td>{item.status}</td>
+            <td>{item.name}</td>
+            <td>
+              <Button variant="warning">
+                <i className="fa-solid fa-pencil"></i>
+                Edit
+              </Button>{" "}
+              <Button variant="danger">
+                <i className="fa-solid fa-trash-can"></i>
+                Delete
+              </Button>
+            </td>
+          </tr>
+        ))}
       </tbody>
     </Table>
   );
--- src/pages/payment-method/paymentMethodAction.js
+++ src/pages/payment-method/paymentMethodAction.js
@@ -7,7 +7,6 @@ import {
 export const fetchPaymentMethods = () => async (dispatch) => {
   //call axios to call api
   const response = await getPaymentMethods();
-  console.log(response);
   //get data and set to state
   response.status === "success" && dispatch(setPaymentMethods(response.result));
 };
